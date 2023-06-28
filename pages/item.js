96b78543ef0c1f131d6a0982900ecced96af4547
realsahilsaini <thisissahilsaini@gmail.com>
2023-06-29 01:43:08 +0530

    Basic UI, Routes, Fetched Main page contents

---
+++ pages/item.js
@@ -0,0 +1,5 @@
+import view from "../utils/view";
+
+export default function Iteon(){
+
+}
---
+++ pages/stories.js
@@ -0,0 +1,42 @@
+import view from '../utils/view.js';
+import Story from '../components/story.js';
+
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  // console.log(stories);
+
+  const hasStories = stories.length > 0;
+
+  view.innerHTML = `<div>
+  ${hasStories ? stories.map((story, i) => Story({...story, index: i+1})).join(''): 'No Stories'}
+  </div>`;  
+}
+
+
+// Edpoint:
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /new
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show
+
+async function getStories(path){
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+
+
+  if(isHomeRoute){
+    path = '/news';
+  }else if(isNewRoute){
+    path = '/newest'
+  }
+
+  const response  =  await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  //To be able to easily resolve this, since we know fetch returns a promise indiciating our data, we can make this async function, and we can await our request that we are making;
+
+  const stories = await response.json();
+
+  return stories;
+
+}
