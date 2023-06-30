30f10303e54f50573f7c321b88e414be527868a4
realsahilsaini <thisissahilsaini@gmail.com>
2023-06-30 19:57:40 +0530

    Favorite page setup

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,5 +1,28 @@
-import view from "../utils/view";
+import view from "../utils/view.js";
+import store from "../store.js";
+import Story from "../components/story.js";
+import checkFavorite from "../utils/checkFavorite.js";
 
 export default function Favorites() {
-    view.innerHTML = `<div>Favorites</div>`  
+
+    const { favorites } = store.getState();
+    const hasFavorites = favorites.length >0;
+
+    view.innerHTML = 
+    `<div>
+    ${hasFavorites ? favorites.map(story=> Story({
+        ...story, 
+        isFavorite: checkFavorite(favorites, story)
+    })).join('') : "Add some Favorites!"}
+    </div>`  
+
+    document.querySelectorAll('.favorite').forEach(favoriteButton => {
+        favoriteButton.addEventListener('click', async function() {
+          const story = JSON.parse(this.dataset.story);  
+          const isFavorited = checkFavorite(favorites, story);
+          store.dispatch({ type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE", payload: { favorite: story } });
+          Favorites();
+        }); 
+     });
+
   }
